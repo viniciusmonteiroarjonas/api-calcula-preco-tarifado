5e92664a73cd6741ef1d5309bfe7fdbe66a0f886
Vinicius Arjonas <viniciusmonteiroarjonasestudos@gmail.com>
2024-09-11 13:33:36 -0300

    feat: commit inicial

---
+++ src/main/java/br/com/api/calculo/CalculoApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.calculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoApplication.class, args);
+	}
+
+}
